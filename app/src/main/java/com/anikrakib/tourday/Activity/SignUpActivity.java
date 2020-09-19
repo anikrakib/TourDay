d30502670fc1328a861a7874c9cf6bfae9dca813
anikrakib <anikrakib3@gmail.com>
2020-09-19 20:21:08 +0600

    Create New User Backend Code Complete

--- app/src/main/java/com/anikrakib/tourday/Activity/SignUpActivity.java
+++ app/src/main/java/com/anikrakib/tourday/Activity/SignUpActivity.java
@@ -4,9 +4,13 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -20,12 +24,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.anikrakib.tourday.R;
+import com.anikrakib.tourday.WebService.RetrofitClient;
 import com.google.android.material.textfield.TextInputLayout;
+import com.pranavpandey.android.dynamic.toasts.DynamicToast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Objects;
 
+import okhttp3.ResponseBody;
 import pl.droidsonroids.gif.GifDrawable;
 import pl.droidsonroids.gif.GifImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -35,8 +49,10 @@ public class SignUpActivity extends AppCompatActivity {
     Button signUpButton;
     private EditText inputUsername, inputEmail, inputPassword;
     private TextInputLayout inputLayoutUsername, inputLayoutEmail, inputLayoutPassword;
+    Dialog postDialog ;
     Toolbar toolbarBack;
     TextView signIn;
+
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -53,9 +69,9 @@ public class SignUpActivity extends AppCompatActivity {
         inputLayoutUsername = findViewById(R.id.input_layout_username);
         inputLayoutEmail = findViewById(R.id.input_layout_email);
         inputLayoutPassword = findViewById(R.id.input_layout_password);
-        inputUsername = findViewById(R.id.input_username);
-        inputEmail = findViewById(R.id.input_email);
-        inputPassword = findViewById(R.id.input_password);
+        inputUsername = findViewById(R.id.userNameSignUp);
+        inputEmail = findViewById(R.id.emailSignUp);
+        inputPassword = findViewById(R.id.passwordSignUp);
 
 
         inputUsername.addTextChangedListener(new MyTextWatcher(inputUsername));
@@ -65,8 +81,9 @@ public class SignUpActivity extends AppCompatActivity {
         signIn = findViewById(R.id.signInTextView);
 
 
+        postDialog = new Dialog(this);
 
-        gifImageView.setVisibility(View.GONE);
+        //gifImageView.setVisibility(View.GONE);
 
 
         setSupportActionBar(toolbarBack);
@@ -85,25 +102,9 @@ public class SignUpActivity extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                signUpBody.setAlpha(0.5f);
-                gifImageView.setVisibility(View.VISIBLE);
-                disableEnableControls(false,signUpBody);
-                //gifDrawable.start();
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        doWork();
-                        activity();
-                        finish();
-                    }
-                });
-                thread.start();
                 submitForm();
             }
         });
-
-
-
 
     }
 
@@ -111,15 +112,7 @@ public class SignUpActivity extends AppCompatActivity {
         onBackPressed();
         return true;
     }
-    public void doWork(){
-        for(int progress=0 ; progress<300 ; progress++){
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+
     public void activity(){
         Intent intent = new Intent(SignUpActivity.this,SignInActivity.class);
         startActivity(intent);
@@ -127,16 +120,6 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
-    //disable and enable ViwGroup
-    private void disableEnableControls(boolean enable, ViewGroup vg){
-        for (int i = 0; i < vg.getChildCount(); i++){
-            View child = vg.getChildAt(i);
-            child.setEnabled(enable);
-            if (child instanceof ViewGroup){
-                disableEnableControls(enable, (ViewGroup)child);
-            }
-        }
-    }
 
     /**
      * Validating form
@@ -145,24 +128,86 @@ public class SignUpActivity extends AppCompatActivity {
         if (!validateUsername()) {
             return;
         }
-
         if (!validateEmail()) {
             return;
         }
-
         if (!validatePassword()) {
             return;
         }
+        loder();
 
-        Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loder(){
+        sendData();
+        postDialog.setContentView(R.layout.gif_view);
+        postDialog.setCancelable(false);
+        postDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        postDialog.show();
+    }
+
+    private void sendData(){
+        setTimeForRunLoder();
+        Call<ResponseBody> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .createUser(inputUsername.getText().toString().trim(),inputEmail.getText().toString().trim(),inputPassword.getText().toString().trim());
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if(response.isSuccessful()){
+                    JSONObject jObjError = null;
+                    try {
+                        jObjError = new JSONObject(response.body().string());
+                        DynamicToast.makeSuccess(getApplicationContext(), jObjError.getJSONArray("username").getString(0)).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    startActivity(new Intent(SignUpActivity.this, MyProfileActivity.class));
+                    finish();
+                }else {
+                    try {
+                        JSONObject jObjError = new JSONObject(response.errorBody().string());
+                        DynamicToast.makeError(getApplicationContext(), jObjError.getJSONArray("username").getString(0)).show();
+                    } catch (Exception e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+//                    Toast.makeText(getApplicationContext(),"A user with that username already exists.",Toast.LENGTH_LONG).show();
+//                    if(response.errorBody()!=null)
+//                        Toast.makeText(SignUpActivity.this,response.errorBody().toString(),Toast.LENGTH_LONG).show();
+                    inputUsername.setText("");
+                    requestFocus(inputUsername);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+    }
+
+
+    public void setTimeForRunLoder(){
+        Handler handler = null;
+        handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                postDialog.cancel();
+                postDialog.dismiss();
+            }
+        }, 2000);
     }
 
     private boolean validateUsername() {
         if (inputUsername.getText().toString().trim().isEmpty()) {
-            inputLayoutUsername.setError(getString(R.string.err_msg_name));
+            inputLayoutUsername.setError(getString(R.string.err_msg_userName1));
             requestFocus(inputUsername);
             return false;
-        } else {
+        }
+        else {
             inputLayoutUsername.setErrorEnabled(false);
         }
 
@@ -184,7 +229,7 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private boolean validatePassword() {
-        if (inputPassword.getText().toString().trim().isEmpty()) {
+        if(inputPassword.getText().toString().length() <=7) {
             inputLayoutPassword.setError(getString(R.string.err_msg_password));
             requestFocus(inputPassword);
             return false;
@@ -210,31 +255,31 @@ public class SignUpActivity extends AppCompatActivity {
 
     private class MyTextWatcher implements TextWatcher {
 
-    private View view;
+        private View view;
 
-    private MyTextWatcher(View view) {
-        this.view = view;
-    }
-
-    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-    }
-
-    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-    }
-
-    public void afterTextChanged(Editable editable) {
-        switch (view.getId()) {
-            case R.id.input_username:
-                validateUsername();
-                break;
-            case R.id.input_email:
-                validateEmail();
-                break;
-            case R.id.input_password:
-                validatePassword();
-                break;
+        private MyTextWatcher(View view) {
+            this.view = view;
         }
-    }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.input_username:
+                    validateUsername();
+                    break;
+                case R.id.input_email:
+                    validateEmail();
+                    break;
+                case R.id.input_password:
+                    validatePassword();
+                    break;
+            }
+        }
 
     }
 }
---
+++ app/src/main/java/com/anikrakib/tourday/WebService/Api.java
@@ -0,0 +1,34 @@
+package com.anikrakib.tourday.WebService;
+
+
+import com.anikrakib.tourday.Models.Token;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface Api {
+
+    @FormUrlEncoded
+    @POST("auth/signup/")
+    Call<ResponseBody> createUser(
+            @Field("username") String username,
+            @Field("email") String email,
+            @Field("password") String password
+    );
+
+    @FormUrlEncoded
+    @POST("auth/login/")
+    Call<Token> logInUser(
+            @Field("username") String username,
+            @Field("password") String password
+    );
+
+    @GET("profile/")
+    Call<ResponseBody> userProfile(@Header("Authorization") String authToken);
+
+}
