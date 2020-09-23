fd536624afb84493d30f392937ae8b80f66c4533
anikrakib <anikrakib3@gmail.com>
2020-09-24 04:11:23 +0600

    Added Specific User Map View and Show warning if user not signin

--- app/src/main/java/com/anikrakib/tourday/Activity/BDMapViewActivity.java
+++ app/src/main/java/com/anikrakib/tourday/Activity/BDMapViewActivity.java
@@ -2,9 +2,16 @@ package com.anikrakib.tourday.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.KeyEvent;
 import android.view.View;
@@ -14,17 +21,22 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.anikrakib.tourday.R;
 
 public class BDMapViewActivity extends AppCompatActivity {
     ImageButton bdMapBackButton;
+    Dialog mDialog;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b_d_map_view);
         bdMapBackButton = findViewById(R.id.bdMapBackButton);
+
+        mDialog = new Dialog(this);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
@@ -38,36 +50,49 @@ public class BDMapViewActivity extends AppCompatActivity {
             }
         });
 
+        SharedPreferences userPref = getApplicationContext().getSharedPreferences("user", Context.MODE_PRIVATE);
+        String username = userPref.getString("userName","");
+
         final WebView webView = findViewById(R.id.webView);
         WebSettings webSettings = webView.getSettings();
         webView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR);
         webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
         WebViewClient webViewClient = new WebViewClient();
         webView.setWebViewClient(webViewClient);
 
-        //for remove specific div
-//        webView.getSettings().setJavaScriptEnabled(true);
-//        webView.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public void onPageFinished(WebView view, String url)
-//            {
-//                webView.loadUrl("javascript:(function() { " +
-//                        "var head = document.getElementsByClassName('btn')[0].style.display='none'; " +
-//                        "})()");
-//            }
-//        });
-
-        webView.loadUrl("https://anikrakib.github.io/BD-Map/");
+        if(username.isEmpty()){
+            showWarningPopUp();
+        }else{
+            webView.loadUrl("https://www.tourday.team/api/map/"+username);
+        }
         // set image scale to fit screen if larger than screen width
         DisplayMetrics displayMetrics = new DisplayMetrics();
         WindowManager wm = (WindowManager) BDMapViewActivity.this.getSystemService(Context.WINDOW_SERVICE);
         wm.getDefaultDisplay().getMetrics(displayMetrics);
-        int screenWidth = displayMetrics.widthPixels;
     }
 
     public void onBackPressed() {
         BDMapViewActivity.this.finish();
     }
 
-
+    public void runWarningPopUp(){
+        Handler handler = null;
+        handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                startActivity(new Intent(BDMapViewActivity.this, SignInActivity.class));
+                finish();
+            }
+        }, 2000);
+        mDialog.dismiss();
+    }
+    private void showWarningPopUp(){
+        mDialog.setContentView(R.layout.custom_warning_pop_up);
+        mDialog.setCancelable(false);
+        mDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        runWarningPopUp();
+        mDialog.show();
+    }
 }
