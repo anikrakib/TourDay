a77fa3ab0a2e129d3a6c09970b1052a3f39a91cd
anik-rakib <anikrakib1998@gmail.com>
2020-10-06 02:30:01 +0600

    Add Blog and YourBlog Fragment in Blog Activity

---
+++ app/src/main/java/com/anikrakib/tourday/Fragment/Blog.java
@@ -0,0 +1,31 @@
+package com.anikrakib.tourday.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anikrakib.tourday.R;
+
+public class Blog extends Fragment {
+
+
+    public Blog() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_blog, container, false);
+    }
+}
