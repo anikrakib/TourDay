ae710e3d0a43fdfe42a8d5eca40f3c15b743b508
anik-rakib <anikrakib3@gmail.com>
2020-10-08 03:02:36 +0600

    Create Blog Design

---
+++ app/src/main/java/com/anikrakib/tourday/Activity/BlogDetailsActivity.java
@@ -0,0 +1,60 @@
+package com.anikrakib.tourday.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import com.anikrakib.tourday.R;
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.squareup.picasso.Picasso;
+import com.tylersuehr.socialtextview.SocialTextView;
+
+public class BlogDetailsActivity extends AppCompatActivity {
+    Intent intent;
+    KenBurnsView blogImageKenBurnsView;
+    TextView blogDetailsTitleTextView,blogDetailsDescriptionTextView,blogDetailsDivisionTextView,blogDetailsDateTextView;
+    SocialTextView blogAuthorName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blog_details);
+
+        blogImageKenBurnsView = findViewById(R.id.blogDetailsImage);
+        blogDetailsTitleTextView = findViewById(R.id.blogDetailsTitleTextView);
+        blogAuthorName = findViewById(R.id.authorUserName);
+        blogDetailsDivisionTextView = findViewById(R.id.blogDetailsLocationTextView);
+        blogDetailsDescriptionTextView = findViewById(R.id.blogDetailsDescriptionTextView);
+        blogDetailsDateTextView = findViewById(R.id.blogDetailsDate);
+
+
+        intent = getIntent();
+        Bundle extras = intent.getExtras();
+
+        assert extras != null;
+        String blogTitle = extras.getString("blogTitle");
+        String blogImage = extras.getString("blogImage");
+        String blogDescription = extras.getString("blogDescription");
+        String division = extras.getString("division");
+        String blogDate = extras.getString("blogDate");
+        //String blogId = extras.getString("blogId");
+        String blogAuthor = extras.getString("blogAuthor");
+
+        Picasso.get().load("https://tourday.team"+blogImage).into(blogImageKenBurnsView);
+        blogAuthorName.setLinkText("@"+blogAuthor);
+        blogDetailsTitleTextView.setText(blogTitle);
+        blogDetailsDivisionTextView.setText(division);
+        blogDetailsDateTextView.setText(blogDate);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            blogDetailsDescriptionTextView.setText(Html.fromHtml(blogDescription, Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            blogDetailsDescriptionTextView.setText(Html.fromHtml(blogDescription));
+        }
+
+    }
+}
--- app/src/main/java/com/anikrakib/tourday/Adapter/AdapterBlog.java
+++ app/src/main/java/com/anikrakib/tourday/Adapter/AdapterBlog.java
@@ -2,16 +2,23 @@ package com.anikrakib.tourday.Adapter;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.anikrakib.tourday.Activity.BlogDetailsActivity;
+import com.anikrakib.tourday.Activity.EventDetailsActivity;
 import com.anikrakib.tourday.Models.BlogItem;
 import com.anikrakib.tourday.Models.PostItem;
 import com.anikrakib.tourday.R;
@@ -54,6 +61,24 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
         holder.blogDate.setText(date);
         holder.blogTitle.setText(title);
         Picasso.get().load("https://tourday.team/"+imageUrl).fit().centerInside().into(holder.blogImage);
+
+        holder.cardViewBlog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // myDialog = new Dialog(mContext);
+                final Intent intent;
+                intent =  new Intent(mContext, BlogDetailsActivity.class);
+                intent.putExtra("blogTitle",title);
+                intent.putExtra("blogImage",imageUrl);
+                intent.putExtra("blogDescription",description);
+                intent.putExtra("division",division);
+                intent.putExtra("blogDate",date);
+                intent.putExtra("blogId",id);
+                intent.putExtra("blogAuthor",author);
+                mContext.startActivity(intent);
+
+            }
+        });
     }
 
     @Override
@@ -65,7 +90,8 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
 
         public RoundedImageView blogImage;
         public TextView blogLocation,blogDate,blogTitle;
-        RelativeLayout relativeLayoutPostItem;
+        LinearLayout blogItemLayout;
+        CardView cardViewBlog;
 
 
         public BlogViewHolder(@NonNull View itemView) {
@@ -75,6 +101,14 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
             blogDate = itemView.findViewById(R.id.blogDate);
             blogLocation = itemView.findViewById(R.id.blogLocation);
             blogTitle = itemView.findViewById(R.id.blogTitle);
+            blogItemLayout = itemView.findViewById(R.id.blogItemLayout);
+            cardViewBlog = itemView.findViewById(R.id.cardViewBlog);
         }
+    }
+    public void  showBlogDetailsPopUp(){
+        myDialog.setContentView(R.layout.blog_details_pop_up);
+
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
