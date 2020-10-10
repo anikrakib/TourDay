bfde1947c8b59c32a52ec1ed674d1e86166d922a
anik-rakib <anikrakib3@gmail.com>
2020-10-10 16:48:01 +0600

    Add Custom TextEditor Using RichText

--- app/src/main/java/com/anikrakib/tourday/Activity/BlogActivity.java
+++ app/src/main/java/com/anikrakib/tourday/Activity/BlogActivity.java
@@ -10,6 +10,7 @@ import androidx.viewpager2.widget.CompositePageTransformer;
 import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
@@ -33,9 +34,13 @@ import com.fiberlink.maas360.android.richtexteditor.RichEditText;
 import com.fiberlink.maas360.android.richtexteditor.RichTextActions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import jp.wasabeef.richeditor.RichEditor;
 
 public class BlogActivity extends AppCompatActivity {
 
@@ -131,10 +136,11 @@ public class BlogActivity extends AppCompatActivity {
     private void createEventPopUp() {
         ImageButton postCloseButton;
         Animation top_to_bottom;
+        RichEditor blogTextEditor;
+        CircleImageView userProfilePicturePopUP;
         final ConstraintLayout createEventLayout;
-        final String[] blogTitleSave = new String[1];
-        final String[] blogDescriptionSave = new String[1];
-        RichTextActions richTextActions ;
+        SharedPreferences userPref =getApplicationContext().getSharedPreferences("user", MODE_PRIVATE);
+        @SuppressLint("CommitPrefEdits") SharedPreferences.Editor editor = userPref.edit();
 
 
         myDialog.setContentView(R.layout.create_blog);
@@ -142,41 +148,31 @@ public class BlogActivity extends AppCompatActivity {
 
         createEventLayout = myDialog.findViewById(R.id.createBlogLayout);
         blogPopUpTitle = myDialog.findViewById(R.id.popupBlogTitle);
-//        blogPopUpDescription = myDialog.findViewById(R.id.popupBlogDescription);
-//        richTextActions = myDialog.findViewById(R.id.rich_text_actions);
+        blogTextEditor = myDialog.findViewById(R.id.popUpBlogTextEditor);
+        userProfilePicturePopUP = myDialog.findViewById(R.id.createBlogPopUpUserProfilePicture);
+
+        blogTextEditor.setEditorFontColor(R.color.color_primary_text);
+        blogTextEditor.setPlaceholder("Write Here your Blog .....");
 
 
+        //set Animation
         top_to_bottom = AnimationUtils.loadAnimation(this, R.anim.top_to_bottom);
 
         // Retrieve and set Event Title and Description from SharedPreferences when again open CreateEvent PopUp
-
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        String eventTitle = sharedPreferences.getString("BlogTitle","");
-        String eventDescription = sharedPreferences.getString("BlogDescription","");
+        String userProfilePicture = userPref.getString("userProfilePicture","");
 
-//        //delete SharedPreference data
-//        SharedPreferences preferences = getSharedPreferences("postTitle", 0);
-//        preferences.edit().remove("postTitle").apply();
 
-        blogPopUpTitle.setText(eventTitle);
+        //set data in element
+        Picasso.get().load("https://tourday.team"+userProfilePicture).into(userProfilePicturePopUP);
 
-        //blogPopUpDescription.setRichTextActionsView(richTextActions);
-        //blogPopUpDescription.setPreviewText(eventDescription);
-        //blogPopUpDescription.setHint("Enter Blog Description");
-        //blogPopUpDescription.setBackgroundColor(R.id.color_white);
-
+        findVieByIdPopUpMethod(myDialog,blogTextEditor);
 
 
         postCloseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Save Event Title and Description in SharedPreferences when close CreateEvent PopUp
 
-//                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-//                SharedPreferences.Editor editor = sharedPreferences.edit();
-//                editor.putString("BlogTitle", blogTitleSave[0]);
-//                editor.putString("BlogDescription", blogDescriptionSave[0]);
-//                editor.apply();
 
                 myDialog.dismiss();
             }
@@ -189,6 +185,200 @@ public class BlogActivity extends AppCompatActivity {
         myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         myDialog.setCancelable(false);
         myDialog.show();
+    }
+
+    public void findVieByIdPopUpMethod(Dialog myDialog,RichEditor blogTextEditor){
+
+        myDialog.findViewById(R.id.action_redo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.redo();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_bold).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setBold();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_Italic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setItalic();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_subscript).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setSubscript();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_superscript).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setSuperscript();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_strikethrough).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setStrikeThrough();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_underline).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setUnderline();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_h1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setHeading(1);
+            }
+        });
+
+        myDialog.findViewById(R.id.action_h2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setHeading(2);
+            }
+        });
+
+        myDialog.findViewById(R.id.action_h3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setHeading(3);
+            }
+        });
+
+        myDialog.findViewById(R.id.action_h4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setHeading(4);
+            }
+        });
+
+        myDialog.findViewById(R.id.action_h5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setHeading(5);
+            }
+        });
+
+        myDialog.findViewById(R.id.action_h6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setHeading(6);
+            }
+        });
+
+        myDialog.findViewById(R.id.action_color).setOnClickListener(new View.OnClickListener() {
+            private boolean isChanged;
+
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setTextColor(isChanged ? Color.BLACK : Color.RED);
+                isChanged = !isChanged;
+            }
+        });
+
+        myDialog.findViewById(R.id.backgroundColor).setOnClickListener(new View.OnClickListener() {
+            private boolean isChanged;
+
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setTextBackgroundColor(isChanged ? Color.TRANSPARENT : Color.YELLOW);
+                isChanged = !isChanged;
+            }
+        });
+
+        myDialog.findViewById(R.id.action_indent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setIndent();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_outdent).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setOutdent();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_align_left).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setAlignLeft();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_align_center).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setAlignCenter();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_align_right).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setAlignRight();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_blockQuote).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setBlockquote();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_unordered_numbered).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setBullets();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_ordered_numbered).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.setNumbers();
+            }
+        });
+
+        myDialog.findViewById(R.id.action_insert_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.insertImage("https://raw.githubusercontent.com/wasabeef/art/master/chip.jpg",
+                        "dachshund", 320);
+            }
+        });
+
+        myDialog.findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.insertYoutubeVideo("https://www.youtube.com/embed/pS5peqApgUA");
+            }
+        });
+
+
+        myDialog.findViewById(R.id.action_insert_link).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                blogTextEditor.insertLink("https://github.com/wasabeef", "wasabeef");
+            }
+        });
+
     }
 
 }
