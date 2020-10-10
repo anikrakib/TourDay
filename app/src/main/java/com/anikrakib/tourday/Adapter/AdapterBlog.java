6899817f3e8f980f8dc68ec34baf467d279af93b
anik-rakib <anikrakib3@gmail.com>
2020-10-10 14:34:12 +0600

    Added more option button in signed in user's Blog

--- app/src/main/java/com/anikrakib/tourday/Adapter/AdapterBlog.java
+++ app/src/main/java/com/anikrakib/tourday/Adapter/AdapterBlog.java
@@ -3,14 +3,17 @@ package com.anikrakib.tourday.Adapter;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupMenu;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -58,10 +61,19 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
         String id = currentItem.getBlogId();
         String author = currentItem.getBlogAuthorName();
 
+        SharedPreferences userPref = mContext.getSharedPreferences("user", Context.MODE_PRIVATE);
+        String userName = userPref.getString("userName","");
+
         holder.blogLocation.setText(division);
         holder.blogDate.setText(date);
         holder.blogTitle.setText(title);
         Picasso.get().load("https://tourday.team/"+imageUrl).fit().centerInside().into(holder.blogImage);
+
+        if(currentItem.getBlogAuthorName().equals(userName)){
+            holder.moreBlogButton.setVisibility(View.VISIBLE);
+        } else {
+            holder.moreBlogButton.setVisibility(View.GONE);
+        }
 
         holder.cardViewBlog.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +89,30 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
                 intent.putExtra("blogAuthor",author);
                 mContext.startActivity(intent);
             }
+        });
+
+        holder.moreBlogButton.setOnClickListener(v->{
+            PopupMenu popupMenu = new PopupMenu(mContext,holder.moreBlogButton);
+            popupMenu.inflate(R.menu.edit_delete_post_menu);
+            popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+
+                    switch (item.getItemId()){
+                        case R.id.editPost: {
+
+                            return true;
+                        }
+                        case R.id.delete_post: {
+
+                            return true;
+                        }
+                    }
+
+                    return false;
+                }
+            });
+            popupMenu.show();
         });
 
         //set Animation in recyclerView Item
@@ -95,6 +131,7 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
         public TextView blogLocation,blogDate,blogTitle;
         LinearLayout blogItemLayout;
         CardView cardViewBlog;
+        ImageView moreBlogButton;
 
 
         public BlogViewHolder(@NonNull View itemView) {
@@ -106,6 +143,7 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
             blogTitle = itemView.findViewById(R.id.blogTitle);
             blogItemLayout = itemView.findViewById(R.id.blogItemLayout);
             cardViewBlog = itemView.findViewById(R.id.cardViewBlog);
+            moreBlogButton = itemView.findViewById(R.id.moreButtonBlog);
         }
     }
 }
