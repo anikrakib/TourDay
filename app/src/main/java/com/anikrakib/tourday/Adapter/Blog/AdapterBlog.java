19b2252ea0dc3a86309233b1f8dce61cc0e4d22c
anik-rakib <anikrakib3@gmail.com>
2020-10-16 19:01:54 +0600

    Added Blog Delete Option in RecyclerView

--- app/src/main/java/com/anikrakib/tourday/Adapter/Blog/AdapterBlog.java
+++ app/src/main/java/com/anikrakib/tourday/Adapter/Blog/AdapterBlog.java
@@ -4,15 +4,19 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
@@ -20,11 +24,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.anikrakib.tourday.Activity.Blog.BlogDetailsActivity;
 import com.anikrakib.tourday.Models.Blog.BlogItem;
+import com.anikrakib.tourday.Models.Blog.DeleteBlogResponse;
 import com.anikrakib.tourday.R;
+import com.anikrakib.tourday.WebService.RetrofitClient;
 import com.makeramen.roundedimageview.RoundedImageView;
+import com.pranavpandey.android.dynamic.toasts.DynamicToast;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder>{
 
@@ -56,6 +67,8 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
         String id = currentItem.getBlogId();
         String author = currentItem.getBlogAuthorName();
 
+        myDialog = new Dialog(mContext);
+
         SharedPreferences userPref = mContext.getSharedPreferences("user", Context.MODE_PRIVATE);
         String userName = userPref.getString("userName","");
 
@@ -86,18 +99,13 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
 
         holder.moreBlogButton.setOnClickListener(v->{
             PopupMenu popupMenu = new PopupMenu(mContext,holder.moreBlogButton);
-            popupMenu.inflate(R.menu.edit_delete_post_menu);
+            popupMenu.inflate(R.menu.delete_post_or_blog_menu);
             popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                 @Override
                 public boolean onMenuItemClick(MenuItem item) {
 
-                    switch (item.getItemId()){
-                        case R.id.editPost: {
-
-                        }
-                        case R.id.delete_post: {
-
-                        }
+                    if (item.getItemId() == R.id.delete_post) {
+                        showDeleteBlogPopUp(String.valueOf(id),position);
                     }
 
                     return false;
@@ -135,5 +143,73 @@ public class AdapterBlog extends RecyclerView.Adapter<AdapterBlog.BlogViewHolder
             cardViewBlog = itemView.findViewById(R.id.cardViewBlog);
             moreBlogButton = itemView.findViewById(R.id.moreButtonBlog);
         }
+    }
+
+
+    // this method for show custom pop user wants to delete pop up or not
+    public void showDeleteBlogPopUp(String blogId,int position) {
+        Button yesButton,noButton;
+        myDialog.setContentView(R.layout.custom_delete_blog_pop_up);
+        yesButton = myDialog.findViewById(R.id.yesButton);
+        noButton = myDialog.findViewById(R.id.noButton);
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteBlog(blogId,position);
+                myDialog.dismiss();
+            }
+        });
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.setCancelable(false);
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
+
+    }
+
+    /* This Method delete specific Blog from server using blogId
+      API->(DELETE) https://www.tourday.team/api/blog/delete/post_id
+      Token is required, Request Token who create a post. Add token with request header.
+      Request:
+        {
+            "Authorization": "Token 93bc86220b144548e5bb507851b6ef7c2a5e1a14",
+        }
+        Response:
+            "Delete Successfully."
+    */
+    public void deleteBlog(String getId,int position){
+        SharedPreferences userPref = mContext.getSharedPreferences("user", Context.MODE_PRIVATE);
+        String token = userPref.getString("token","");
+
+        Call<DeleteBlogResponse> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .deleteBlog("Token "+token,getId);
+
+        call.enqueue(new Callback<DeleteBlogResponse>() {
+            @Override
+            public void onResponse(Call<DeleteBlogResponse> call, Response<DeleteBlogResponse> response) {
+                if(response.isSuccessful()){
+                    mBlogItemList.remove(position);
+                    notifyItemRemoved(position);
+                    assert response.body() != null;
+                    String success = response.body().getSuccess();
+                    DynamicToast.makeSuccess(mContext, success).show();
+
+                }else{
+                    DynamicToast.makeError(mContext, "Something Wrong !!").show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<DeleteBlogResponse> call, Throwable t) {
+                Toast.makeText(mContext, t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
