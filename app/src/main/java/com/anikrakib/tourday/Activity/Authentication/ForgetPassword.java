1c62104eba7c033239996db74b62424982d4eb8d
anik-rakib <anikrakib3@gmail.com>
2020-10-24 02:00:52 +0600

    Forget Password Activity Design Complete

---
+++ app/src/main/java/com/anikrakib/tourday/Activity/Authentication/ForgetPassword.java
@@ -0,0 +1,16 @@
+package com.anikrakib.tourday.Activity.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.anikrakib.tourday.R;
+
+public class ForgetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
