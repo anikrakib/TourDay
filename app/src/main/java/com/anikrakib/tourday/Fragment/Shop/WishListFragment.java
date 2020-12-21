a55a686bf2286584fa732d2234a5fb5ef43eea64
anikrakib <anikrakib1998@gmail.com>
2020-12-21 23:29:47 +0600

    Wish List Complete

--- app/src/main/java/com/anikrakib/tourday/Fragment/Shop/WishListFragment.java
+++ app/src/main/java/com/anikrakib/tourday/Fragment/Shop/WishListFragment.java
@@ -1,8 +1,11 @@
 package com.anikrakib.tourday.Fragment.Shop;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
@@ -18,12 +21,15 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.anikrakib.tourday.Adapter.Search.AdapterAllEventSearch;
 import com.anikrakib.tourday.Adapter.Shop.AdapterAllWishList;
 import com.anikrakib.tourday.R;
 import com.anikrakib.tourday.RoomDatabse.MyDatabase;
 import com.anikrakib.tourday.RoomDatabse.ProductWishListDatabaseTable;
+import com.anikrakib.tourday.Utils.CheckInternet;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,13 +45,19 @@ public class WishListFragment extends Fragment {
     CardView notFound;
     TextView emptyPostTextView1,emptyPostTextView2;
     ImageView removeAll;
+    public boolean checkInternet,isLoggedIn;
+    Dialog postDialog ;
+    String userId;
+    LottieAnimationView lottieAnimationView;
+
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
-    @SuppressLint("SetTextI18n")
+    @SuppressLint({"SetTextI18n", "CutPasteId"})
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -58,34 +70,20 @@ public class WishListFragment extends Fragment {
         notFound = view.findViewById(R.id.emptyCardView);
         wishListMainLayout = view.findViewById(R.id.wishListMainLayout);
         removeAll = view.findViewById(R.id.removeAllWishListItem);
+        lottieAnimationView = view.findViewById(R.id.noResultGifView);
 
         myDatabase = MyDatabase.getInstance(getContext());
+        checkInternet = CheckInternet.isConnected(getContext());
+        postDialog = new Dialog(getActivity());
+
 
         SharedPreferences userPref = requireContext().getSharedPreferences("user", Context.MODE_PRIVATE);
-        String userId = userPref.getString("id","");
-        boolean isLoggedIn = userPref.getBoolean("isLoggedIn",false);
+        userId = userPref.getString("id","");
+        isLoggedIn = userPref.getBoolean("isLoggedIn",false);
 
         getAllProductWishList(userId);
+        checkInternetAndLoggedInOrNot();
 
-        if(isLoggedIn){
-            assert userId != null;
-            productWishListDatabaseTables = myDatabase.favouriteEventDatabaseDao().getAllWishProduct(userId);
-            if(productWishListDatabaseTables.isEmpty()){
-                notFound.setVisibility(View.VISIBLE);
-                wishListMainLayout.setVisibility(View.GONE);
-                emptyPostTextView2.setText("Tap the heart shape and add wish list");
-                emptyPostTextView1.setText("You have no wish list item yet !!");
-            }
-            else{
-                notFound.setVisibility(View.GONE);
-                wishListMainLayout.setVisibility(View.VISIBLE);
-            }
-        }else{
-            notFound.setVisibility(View.VISIBLE);
-            wishListMainLayout.setVisibility(View.GONE);
-            emptyPostTextView2.setText("If you have no account, then create an account");
-            emptyPostTextView1.setText("Sign In Required");
-        }
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -108,6 +106,40 @@ public class WishListFragment extends Fragment {
         });
 
         return view;
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void checkInternetAndLoggedInOrNot(){
+        if(!checkInternet){
+
+            notFound.setVisibility(View.VISIBLE);
+            lottieAnimationView.setAnimation(R.raw.no_internet);
+            emptyPostTextView2.setText("Check You Data Connection or Wifi Connection");
+            emptyPostTextView1.setText("Sorry Internet Not Connected");
+            wishListMainLayout.setVisibility(View.GONE);
+
+
+        }else{
+            if(isLoggedIn){
+                assert userId != null;
+                productWishListDatabaseTables = myDatabase.favouriteEventDatabaseDao().getAllWishProduct(userId);
+                if(productWishListDatabaseTables.isEmpty()){
+                    notFound.setVisibility(View.VISIBLE);
+                    wishListMainLayout.setVisibility(View.GONE);
+                    emptyPostTextView2.setText("Tap the heart shape and add wish list");
+                    emptyPostTextView1.setText("You have no wish list item yet !!");
+                }
+                else{
+                    notFound.setVisibility(View.GONE);
+                    wishListMainLayout.setVisibility(View.VISIBLE);
+                }
+            }else{
+                notFound.setVisibility(View.VISIBLE);
+                wishListMainLayout.setVisibility(View.GONE);
+                emptyPostTextView2.setText("If you have no account, then create an account");
+                emptyPostTextView1.setText("Sign In Required");
+            }
+        }
     }
 
     public void getAllProductWishList(String currentUserId){
